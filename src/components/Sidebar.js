9eeafb92d05ac7a5b4bfa950f355bb275fb65ea4
compactly8274 <philjnewman@hotmail.com>
2026-05-14 23:27:30 -0700

    Fix mobile hamburger menu using React state instead of Bootstrap JS
    Bootstrap JS (and jQuery) was never loaded, so data-toggle=collapse was
    a no-op. Replaced with React-controlled isOpen state. Menu also closes
    automatically when a nav link is tapped.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -17,11 +17,22 @@ export class Sidebar extends Component {
         { content: 'Interests', href: 'interests' },
         { content: 'Certifications', href: 'awards' },
       ],
+      isOpen: false,
     };
+    this.toggleMenu = this.toggleMenu.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
+  }
+
+  toggleMenu() {
+    this.setState(state => ({ isOpen: !state.isOpen }));
+  }
+
+  closeMenu() {
+    this.setState({ isOpen: false });
   }
 
   render() {
-    const { tabs } = this.state;
+    const { tabs, isOpen } = this.state;
     return (
       <nav
         className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top"
@@ -42,27 +53,32 @@ export class Sidebar extends Component {
         <button
           className="navbar-toggler"
           type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
+          onClick={this.toggleMenu}
+          aria-expanded={isOpen}
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div
+          className={`navbar-collapse${isOpen ? '' : ' collapse'}`}
+          id="navbarSupportedContent"
+        >
           <Scrollspy
             items={tabs.map(s => s.href)}
             currentClassName="active"
             offset={-300}
             className="navbar-nav"
           >
-            {tabs.map((tab, i) => {
+            {tabs.map(tab => {
               const { href, content } = tab;
               return (
                 <li className="nav-item" key={href}>
                   <Scroll type="id" element={href}>
-                    <a className="nav-link" href={`#${href}`}>
+                    <a
+                      className="nav-link"
+                      href={`#${href}`}
+                      onClick={this.closeMenu}
+                    >
                       {content}
                     </a>
                   </Scroll>
